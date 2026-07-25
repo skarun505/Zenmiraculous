9a703a9a90a6830d14ccca1ca458bfaf7aac542f
skarun505 <skarunkumar460@gmail.com>
2026-07-25 18:38:06 +0530

    Fix white screen: set framework=null in vercel.json, clean unused import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,7 +7,7 @@ import {
   HeadContent,
   Scripts,
 } from "@tanstack/react-router";
-import { useEffect, type ReactNode } from "react";
+import { type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 
